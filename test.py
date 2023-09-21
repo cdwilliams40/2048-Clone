e22b85417e786d79813e404e8c02e91094915e06
cdwilliams40 <git@mailman.mozmail.com>
2023-09-21 12:56:13 -0500

    Initial test of pygame

---
+++ test.py
@@ -0,0 +1,26 @@
+# import the pygame module, so you can use it
+import pygame
+import sys
+ 
+# define a main function
+def main():
+     
+    # initialize the pygame module
+    pygame.init()
+    pygame.display.set_caption("2048")
+     
+    # create a surface on screen that has the size of 240 x 180
+    screen = pygame.display.set_mode((500,500))
+     
+    # define a variable to control the main loop
+    running = True
+     
+    # main loop
+    while running:
+        # event handling, gets all event from the event queue
+        for event in pygame.event.get():
+            # only do something if the event is of type QUIT
+            if event.type == pygame.QUIT:
+                # change the value to False, to exit the main loop
+                pygame.quit()
+                running = False
